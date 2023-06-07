ff665a7fb3cf606017b51839a8784af081435cb2
Antony Lao <antony.lao@laposte.net>
2023-06-07 14:31:44 +0000

    Optimization of queries

--- database_persistence.rb
+++ database_persistence.rb
@@ -16,14 +16,20 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<~SQL
+      SELECT l.*,
+             COUNT(t.id) AS todos_count,
+             COUNT(NULLIF(t.completed, true)) AS todos_remaining_count
+        FROM lists AS l
+             LEFT JOIN todos AS t
+             ON t.list_id = l.id
+       WHERE l.id = $1
+       GROUP BY l.id
+       ORDER BY l.name;
+    SQL
     result = query(sql, id)
 
-    tuple = result.first
-
-    list_id = tuple["id"].to_i
-    todos = find_todos_for_list(list_id)
-    { id: list_id, name: tuple["name"], todos: todos }
+    tuple_to_list_hash(result.first)
   end
 
   def find_todo(list_id, todo_id)
@@ -36,13 +42,20 @@ class DatabasePersistence
   end
 
   def all_lists
-    sql = "SELECT * FROM lists"
+    sql = <<~SQL
+      SELECT l.*,
+             COUNT(t.id) AS todos_count,
+             COUNT(NULLIF(t.completed, true)) AS todos_remaining_count
+        FROM lists AS l
+             LEFT JOIN todos AS t
+             ON t.list_id = l.id
+       GROUP BY l.id
+       ORDER BY l.name;
+    SQL
     result = query(sql)
 
     result.map do |tuple|
-      list_id = tuple["id"].to_i
-      todos = find_todos_for_list(list_id)
-      { id: list_id, name: tuple["name"], todos: todos }
+      tuple_to_list_hash(tuple)
     end
   end
 
@@ -85,19 +98,24 @@ class DatabasePersistence
     @db.close
   end
 
-  private
-
   def find_todos_for_list(list_id)
-    todo_sql = "SELECT * FROM todos WHERE list_id = $1"
+    todos_sql = "SELECT * FROM todos WHERE list_id = $1 ORDER BY name"
 
-    todos_result = query(todo_sql, list_id)
+    todos_result = query(todos_sql, list_id)
 
     todos_hash = todos_result.map do |todo_tuple|
       { id: todo_tuple["id"].to_i,
         name: todo_tuple["name"],
         completed: todo_tuple["completed"] == "t" }
     end
+  end
 
-    todos_hash.sort_by { |todo_hash| todo_hash[:id] }
+  private
+
+  def tuple_to_list_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 end
--- todo.rb
+++ todo.rb
@@ -17,19 +17,11 @@ end
 
 helpers do
   def list_complete?(list)
-    todos_count(list) > 0 && todos_remaining_count(list) == 0
+    list[:todos_count] > 0 && list[:todos_remaining_count] == 0
   end
 
   def list_class(list)
     "complete" if list_complete?(list)
-  end
-
-  def todos_count(list)
-    list[:todos].size
-  end
-
-  def todos_remaining_count(list)
-    list[:todos].select { |todo| !todo[:completed] }.size
   end
 
   def sort_lists(lists, &block)
@@ -119,6 +111,7 @@ end
 get "/lists/:id" do
   @list_id = params[:id].to_i
   @list = load_list(@list_id)
+  @todos = @storage.find_todos_for_list(@list_id)
   erb :list, layout: :layout
 end
 
@@ -187,7 +180,7 @@ post "/lists/:list_id/todos/:todo_id/delete" do
   list_id = params[:list_id].to_i
   list = load_list(list_id)
   todo_id = params[:todo_id].to_i
-  deleted_todo = list[:todos].find { |todo| todo[:id] == todo_id }
+  deleted_todo = @storage.find_todo(list_id, todo_id)
 
   @storage.delete_todo_from_list(list_id, todo_id)
 
